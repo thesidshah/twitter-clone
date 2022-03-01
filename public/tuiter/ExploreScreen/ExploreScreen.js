2709285fea5208f77421badece0c24cc010e8e1d
Siddhant Shah <siddhant997@gmail.com>
2022-03-01 01:59:08 -0500

    Assignment5

--- public/tuiter/ExploreScreen/ExploreScreen.js
+++ public/tuiter/ExploreScreen/ExploreScreen.js
@@ -6,7 +6,7 @@ import WhoToFollowList from "../WhoToFollowList";
     $('#wd-explore').append(`
         <div class="row mt-2">
         <div class="col-2 col-md-2 col-lg-2 col-xl-2 ">
-            ${NavigationSidebar()}
+            ${NavigationSidebar("Explore")}
         </div>
             ${ExploreComponent()}
         <div class="d-sm-none d-md-none d-lg-block col-lg-4 col-xl-4">
--- public/tuiter/PostList/PostItem.js
+++ public/tuiter/PostList/PostItem.js
@@ -1,6 +1,6 @@
 const PostItem = (post) => {
     return(`
-            <li class="list-group-item">
+<!--            <li class="list-group-item">-->
 		    	<img src="${post.image}" class="wd-display-img"/>
 		    	<a href="#" class="wd-topic-heading">${post.author}  </a><i class="fa fa-check-circle"></i><a href="#" class="wd-follow-handle">@${post.handle}</a>
 		    	<span class="wd-topic-time"> - ${post.time}</span><br>
@@ -15,7 +15,7 @@ const PostItem = (post) => {
 					<a href="#" class="wd-icon-link-heart">	&#10084;  2</a>
 					<a href="#" class="wd-icon-link"><i class="fa fa-upload" aria-hidden="true"></i></a>
 				</div>
-		    </li>
+<!--		    </li>-->
     `);
 }
 export default PostItem;
