871ffc29cf5d96cb021c816ef637af78894dcfda
Siddhant Shah <siddhant997@gmail.com>
2022-03-28 23:08:37 -0400

    home page is now working

--- src/App.js
+++ src/App.js
@@ -67,7 +67,7 @@ function App() {
                     <Route path="/labs"
                            element={<Labs/>}/>
                     <Route path="/tuiter"
-                           element={<HomeScreen/>}>
+                           element={<Tuiter/>}>
                     <Route path="home" element={<HomeScreen/>}/>
                     <Route path="explore" element={<ExploreScreen/>}/>
                     <Route path = "testHome" element = {<TestHome/>}/>
