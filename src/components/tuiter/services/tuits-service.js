58e27153da4799a0b6d0686ecbb7f8c8e79f7255
Siddhant Shah <siddhant997@gmail.com>
2022-04-06 22:56:57 -0400

    Final A8 commit

--- src/components/tuiter/services/tuits-service.js
+++ src/components/tuiter/services/tuits-service.js
@@ -7,7 +7,9 @@ import axios from 'axios';
 *  tuits array sent back from findAllTuits in tuits-controller.js and it's embedded in the response's data property.
 * */
 // const TUITS_API = 'http://localhost:4000/api/tuits';
-const TUITS_API = 'https://full-stack-sid.herokuapp.com/api/tuits';
+// const TUITS_API = 'https://full-stack-sid.herokuapp.com/api/tuits';
+const API_BASE = process.env.REACT_APP_API_BASE;
+const TUITS_API = `${API_BASE}/tuits`;
 export const createTuit = async (tuit) => {
     const response = await axios.post(TUITS_API, tuit);
     return response.data;
